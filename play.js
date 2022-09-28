fcf81d611b2e0683276f949e91b3b00e338b9697
TripEEE <evan.trippel@gmail.com>
2022-09-28 14:19:47 -0400

    Moved setupInput to an external file and exported said file into play.js

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require("./client.js")
+const { setupInput } = require("./input.js")
 
 console.log("Connecting ...");
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
-  stdin.on("data", handleUserInput)
-  return stdin;
-};
 connect();
 setupInput()
